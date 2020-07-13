de26d1ae6769a8141eee053af11b0b4e5fe03d92
Christina Starr <christinastarr19@gmail.com>
2020-07-12 23:06:28 -0400

    set up config, db folders, package.json and lock, server.js

---
+++ config/orm.js
@@ -0,0 +1,14 @@
+var connection = require("./connection");
+
+//Create my methods here
+var orm = {
+
+
+//Create selectAll()
+
+//Create insertOne()
+
+//Create updateOne()
+}
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, devoured) VALUES ("Impossible Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Beyond Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Varsity Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Fat Elvis Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Freaky Tiki Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Loco Moco Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("All-American Patty Melt", false);
