2247ff6ba7d1d01d96d7626f9ca3903ccd09d900
Christina Starr <christinastarr19@gmail.com>
2020-07-13 20:17:38 -0400

    set up public and views folders.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,19 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var burger = require("../models/burger");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    
+    console.log(hbsObject);
+    res.render("index", {burgers: data});
+  });
+});
+
+
+
+module.exports = router;
