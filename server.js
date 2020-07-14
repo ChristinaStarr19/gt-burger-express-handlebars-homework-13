1acbd31dac18f77c7ec2d12e35aec70a45af5131
Christina Starr <christinastarr19@gmail.com>
2020-07-14 09:27:16 -0400

    updated title of page to 'Eat-Da-Burger'.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var PORT = process.env.PORT || 8080;
 
 var app = express();
+var routes = require("./controllers/burgers_controller.js");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -20,7 +21,6 @@ app.set("view engine", "handlebars");
 
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
