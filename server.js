69379ef0d8881e483bdeb7475d5ff42e5e634a6b
Christina Starr <christinastarr19@gmail.com>
2020-07-13 23:12:12 -0400

    set up all connection properly. works on localhost. get route is rendering.

--- server.js
+++ server.js
@@ -18,14 +18,11 @@ const { response } = require("express");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.get("/", function(req, res) {
-  response.send("Hello World!");
-})
 
 // Import routes and give the server access to them.
-// var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/burgers_controller.js");
 
-// app.use(routes);
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
