2d027c458a57b6691949bf480ae216ff3452a768
Christina Starr <christinastarr19@gmail.com>
2020-07-13 22:33:26 -0400

    added back connection, orm, burgers_controller,burger, index and main files. and updated connection to include JAWSDB

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var burger = require("../models/burger");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    console.log({ burgers: data});
+    res.render("index", { burgers: data});
+  });
+});
+
+
+// Export routes for server.js to use.
+module.exports = router;
